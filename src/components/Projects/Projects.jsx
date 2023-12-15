3b016631c8de4434cda0acbdd8633b06f694eda0
alexislam0207 <alexislam0207@gmail.com>
2023-12-15 14:50:16 +0000

    update

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import newServer from "../../Assets/Projects/news-server.png";
-import comingSoon from "../../Assets/Projects/coming-soon.png";
+import newServer from "../../Assets/Projects/new-server.png";
+import comingSoon from "../../Assets/Projects/northcoders-news.png";
 
 function Projects() {
   return (
@@ -22,8 +22,7 @@ function Projects() {
               imgPath={newServer}
               isBlog={false}
               title="News-server"
-              description="A comprehensive and versatile back-end server for news data, users
-              can access and amend data about articles and comments etc."
+              description="A comprehensive back-end news server. Designed with test-driven development to ensure reliability."
               skills="PSQL | Express | Jest | Supertest | Javascript"
               ghLink="https://github.com/alexislam0207/news-server"
               demoLink="https://alexis-news-server.onrender.com/api/"
@@ -35,7 +34,7 @@ function Projects() {
               imgPath={comingSoon}
               isBlog={false}
               title="Northcoders-news"
-              description="A front-end website that is implemented based on the news server, designed with UI, UX, 
+              description="A front-end website that is implemented based on the news server. Designed with UI, UX, 
               and responsiveness in mind."
               skills="React.js | Javascript | HTML | CSS"
               ghLink="https://github.com/alexislam0207/Northcoders-news"
